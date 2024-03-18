55eb58948d8499690105f6a1569569841b9394ab
Karthik <karthik@gmail.com>
2024-03-18 21:59:38 +0530

    add function concept

---
+++ Function.ts
@@ -0,0 +1,16 @@
+// TypeScript Functions
+
+/**
+ * @function The type of the value returned by the function can be explicitly defined
+ * @syntax  
+ * function function_name (para: type, para: type) : return type { 
+ *      // Statement Code
+ *  }
+ */
+
+function sum(a: number, b: number): string {
+    return `${a} + ${b} = ${a+b}`;
+}
+
+
+console.log(sum(20, 30));
